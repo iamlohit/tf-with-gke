a3435ded465cde19ebb149078d15dbfdfc199ded
Lohit Jethwani <jethwanilohit@gmail.com>
2024-10-26 19:01:16 +0530

    Added var for services

--- 01-TF-Project-Creation/variables.tf
+++ 01-TF-Project-Creation/variables.tf
@@ -12,3 +12,8 @@ variable "billing_account_id" {
   description = "The billing account ID"
   type        = string
 }
+
+variable "services" {
+  type = list(string)
+  default = [ "compute.googleapis.com", "storage.googleapis.com", "container.googleapis.com" ]
+}
