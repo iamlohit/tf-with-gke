a558a331a50009621ad916a7e700fbcc7980b35c
Lohit Jethwani <jethwanilohit@gmail.com>
2024-10-26 18:57:02 +0530

    Added tf and provider version constraints

--- 01-TF-Project-Creation/provider-versions.tf
+++ 01-TF-Project-Creation/provider-versions.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">=1.8.5"
+  required_version = "~>1.8.5"
   required_providers {
     google = {
-      source = "hashicorp/google"
-      version = ">=5.33.0"
+      source  = "hashicorp/google"
+      version = "~>5.33.0"
     }
   }
 }
 
 provider "google" {
   project = var.project_id
-  region = "us-central1"
+  region  = "us-central1"
 }
