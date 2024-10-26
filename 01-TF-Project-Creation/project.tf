17e58f2b7fbba9ca77752df73b31d94e6f6f843d
Lohit Jethwani <jethwanilohit@gmail.com>
2024-10-26 19:04:23 +0530

    Added for_each function to enable service api's

--- 01-TF-Project-Creation/project.tf
+++ 01-TF-Project-Creation/project.tf
@@ -3,3 +3,9 @@ resource "google_project" "my_project" {
   project_id      = var.project_id
   billing_account = var.billing_account_id
 }
+
+resource "google_project_service" "name" {
+  for_each = toset(var.services)
+  project = google_project.my_project.project_id
+  service = each.value
+}
