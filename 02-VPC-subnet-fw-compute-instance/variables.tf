7f1e003085ba0c06f3fa0ab29797fa134a86696a
Lohit Jethwani <jethwanilohit@gmail.com>
2024-10-26 20:05:28 +0530

    Created VPC and subnet

---
+++ 02-VPC-subnet-fw-compute-instance/variables.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+  description = "The name of the project"
+  type        = string
+}
+
+variable "project_id" {
+  description = "The unique project ID"
+  type        = string
+}
+
+variable "billing_account_id" {
+  description = "The billing account ID"
+  type        = string
+}
+
+variable "services" {
+  type    = list(string)
+  default = ["compute.googleapis.com", "storage.googleapis.com", "container.googleapis.com", "cloudresourcemanager.googleapis.com"]
+}
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
